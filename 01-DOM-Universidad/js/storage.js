9df5e8b518d6f8af21fb91d95e493851ada6f6fb
Marcos Crisostomo <>
2019-12-27 23:30:37 -0600

    Storage

---
+++ 01-DOM-Universidad/js/storage.js
@@ -0,0 +1,8 @@
+// agregar local storage
+localStorage.setItem('variable', 'descripcion localStorage');
+localStorage.removeItem('variable');
+localStorage.clear();
+const miVar = localStorage.getItem('variable');
+
+// Session Storage
+sessionStorage.setItem('variable', 'descripcion sessionStorage' );
