4d999a5dbd8c0c6942d65d0684e33a81705cc421
Manuel Crisostomo <luckybstrd.code@gmail.com>
2020-08-29 10:32:48 -0500

    Update Projects

--- 01-DOM-Universidad/js/storage.js
+++
@@ -1,8 +0,0 @@
-// agregar local storage
-localStorage.setItem('variable', 'descripcion localStorage');
-localStorage.removeItem('variable');
-localStorage.clear();
-const miVar = localStorage.getItem('variable');
-
-// Session Storage
-sessionStorage.setItem('variable', 'descripcion sessionStorage' );
